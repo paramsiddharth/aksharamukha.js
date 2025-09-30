b2be1de8c6b3ac1e28d73ea9e0cf5895a8b5ccfa
Param Siddharth <contact@paramsid.com>
2025-10-01 02:11:53 +0530

    Format file
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,35 @@
-import js from '@eslint/js';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
-  // Global ignores
-  {
-	ignores: [
-		'dist/',
-		'node_modules/'
-	]
-  },
-  
-  // Base JavaScript config
-  js.configs.recommended,
-  
-  // TypeScript files config
-  {
-	files: ['**/*.ts', '**/*.tsx', '/*.js', '/*.ts'],
-	languageOptions: {
-		parser: tsParser,
-		parserOptions: {
-	ecmaVersion: 'latest',
-	sourceType: 'module'
-		}
+	{
+		ignores: ["dist/", "node_modules/"],
 	},
-	plugins: {
-		'@typescript-eslint': tseslint
-	},
-	rules: {
-		// Base recommended TypeScript rules
-		...tseslint.configs.recommended.rules,
-	
-		// Style rules
-		'quotes': ['error', 'single', { 'allowTemplateLiterals': true }], // enforce single quotes
-		'indent': ['error', 'tab'], // use tabs, not spaces
-		'eol-last': ['error', 'never'], // forbid newline at EOF
+	js.configs.recommended,
+	{
+		files: ["**/*.ts", "**/*.tsx", "/*.js", "/*.ts"],
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				ecmaVersion: "latest",
+				sourceType: "module",
+			},
+		},
+		plugins: {
+			"@typescript-eslint": tseslint,
+		},
+		rules: {
+			...tseslint.configs.recommended.rules,
 
-		// TS-specific
-		'@typescript-eslint/no-unused-vars': ['warn'],
-		'no-console': 'off'
-	}
-  }
+			// Style rules
+			quotes: ["error", "single", { allowTemplateLiterals: true }], // enforce single quotes
+			indent: ["error", "tab"], // use tabs, not spaces
+			"eol-last": ["error", "never"], // forbid newline at EOF
+
+			// TS-specific
+			"@typescript-eslint/no-unused-vars": ["warn"],
+			"no-console": "off",
+		},
+	},
 ];
